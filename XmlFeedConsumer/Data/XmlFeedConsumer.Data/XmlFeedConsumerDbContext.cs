5818ab0604b44a35b4d4b9884185ad437cbbbc90
itplamen@gmail.com <itplamen@gmail.com>
2017-04-19 07:55:48 +0300

    Created XmlFeedConsumer.Data.Common, XmlFeedConsumer.Data.Models, and XmlFeedConsumer.Data projects and also XmlFeedConsumerDbContext class

---
+++ XmlFeedConsumer/Data/XmlFeedConsumer.Data/XmlFeedConsumerDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace XmlFeedConsumer.Data
+{
+    using System.Data.Entity;
+
+    public class XmlFeedConsumerDbContext : DbContext
+    {
+        public XmlFeedConsumerDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static XmlFeedConsumerDbContext Create()
+        {
+            return new XmlFeedConsumerDbContext();
+        }
+    }
+}
