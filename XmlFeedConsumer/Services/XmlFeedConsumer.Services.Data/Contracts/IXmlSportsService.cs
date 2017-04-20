26566e800c3f8ff21a672ef5cd5d01c3fc5cf31a
itplamen@gmail.com <itplamen@gmail.com>
2017-04-20 17:24:18 +0300

    Created IXmlSportsService and XmlSportsService

---
+++ XmlFeedConsumer/Services/XmlFeedConsumer.Services.Data/Contracts/IXmlSportsService.cs
@@ -0,0 +1,25 @@
+﻿namespace XmlFeedConsumer.Services.Data.Contracts
+{
+    using System.Linq;
+
+    using XmlFeedConsumer.Data.Models;
+
+    public interface IXmlSportsService
+    {
+        int Add(XmlSport xmlSport);
+
+        XmlSport Get(int id);
+
+        IQueryable<XmlSport> GetAsQueryable(int id);
+
+        IQueryable<XmlSport> All();
+
+        IQueryable<XmlSport> AllWithDeleted();
+
+        XmlSport Update(int id, XmlSport xmlSport);
+
+        XmlSport Delete(int id);
+
+        bool HardDelete(int id);
+    }
+}
