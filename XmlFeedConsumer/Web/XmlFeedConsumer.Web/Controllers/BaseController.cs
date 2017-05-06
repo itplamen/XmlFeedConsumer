35eaec78ad8c91eb104e6e7ae1994caf08cad448
itplamen@gmail.com <itplamen@gmail.com>
2017-05-06 23:34:05 +0300

    Added BaseController

---
+++ XmlFeedConsumer/Web/XmlFeedConsumer.Web/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿namespace XmlFeedConsumer.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using Ninject;
+
+    using Services.Utils.Contracts;
+
+    public abstract class BaseController : Controller
+    {
+        [Inject]
+        public ICacheService Cache { protected get; set; }
+    }
+}
