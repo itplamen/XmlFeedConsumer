35c13768f96fb5cf7e0248e4841a3460a619313d
itplamen@gmail.com <itplamen@gmail.com>
2017-05-10 00:32:27 +0300

    Installed and configured jquery.unobtrusive-ajax

--- XmlFeedConsumer/Web/XmlFeedConsumer.Web/Scripts/_references.js
+++ XmlFeedConsumer/Web/XmlFeedConsumer.Web/Scripts/_references.js
@@ -2,6 +2,7 @@
 /// <reference path="bootstrap.js" />
 /// <reference path="date.format.js" />
 /// <reference path="jquery.signalr-2.2.1.min.js" />
+/// <reference path="jquery.unobtrusive-ajax.min.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="jquery-1.12.4.js" />
