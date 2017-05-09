6c5144c2e7f4998858e5f582cd3111402efb41e2
itplamen@gmail.com <itplamen@gmail.com>
2017-05-10 00:00:11 +0300

    Created AjaxOnlyAttribute

---
+++ XmlFeedConsumer/Web/XmlFeedConsumer.Web/Attributes/AjaxOnlyAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace XmlFeedConsumer.Web.Attributes
+{
+    using System;
+    using System.Web.Mvc;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AjaxOnlyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                base.OnActionExecuting(filterContext);
+            }
+            else
+            {
+                filterContext.HttpContext.Response.StatusCode = 404;
+                filterContext.Result = new HttpNotFoundResult();
+            }
+        }
+    }
+}
