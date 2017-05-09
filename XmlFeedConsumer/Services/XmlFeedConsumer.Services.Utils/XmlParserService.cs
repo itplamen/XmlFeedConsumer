09bd96f404f4f63703068d119d72121216871f5f
itplamen@gmail.com <itplamen@gmail.com>
2017-05-09 22:01:32 +0300

    Fixed bug in XmlParserService

--- XmlFeedConsumer/Services/XmlFeedConsumer.Services.Utils/XmlParserService.cs
+++ XmlFeedConsumer/Services/XmlFeedConsumer.Services.Utils/XmlParserService.cs
@@ -84,10 +84,13 @@
         {
             XDocument xmlDoc = XDocument.Load(uri);
 
+            //return xmlDoc.Descendants("Match")
+            //    .Where(m => ((Convert.ToDateTime(m.Attribute("StartDate").Value)) - DateTime.Now).TotalHours >= 0 &&
+            //        ((Convert.ToDateTime(m.Attribute("StartDate").Value)) - DateTime.Now).TotalHours <= TwentyFourHours &&
+            //        m.Elements("Bet").Elements("Odd").Any());
+
             return xmlDoc.Descendants("Match")
-                .Where(m => ((Convert.ToDateTime(m.Attribute("StartDate").Value)) - DateTime.Now).TotalHours >= 0 &&
-                    ((Convert.ToDateTime(m.Attribute("StartDate").Value)) - DateTime.Now).TotalHours <= TwentyFourHours &&
-                    m.Elements("Bet").Elements("Odd").Any());
+                .Where(m => m.Elements("Bet").Elements("Odd").Any());
         }
     }
 }
