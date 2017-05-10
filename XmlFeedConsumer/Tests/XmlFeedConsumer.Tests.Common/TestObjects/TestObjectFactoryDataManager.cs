5d7ad0e47ae70ebfc92bb1478b0df5e85c3b91f9
itplamen@gmail.com <itplamen@gmail.com>
2017-05-10 23:30:56 +0300

    Added TestObjectFactoryDataManager

---
+++ XmlFeedConsumer/Tests/XmlFeedConsumer.Tests.Common/TestObjects/TestObjectFactoryDataManager.cs
@@ -0,0 +1,26 @@
+﻿namespace XmlFeedConsumer.Tests.Common.TestObjects
+{
+    using Moq;
+
+    using Web.Common.Contracts;
+
+    public static class TestObjectFactoryDataManager
+    {
+        private const int ValidId = 1;
+
+        public static IDataManager GetDataManager()
+        {
+            var dataManager = new Mock<IDataManager>();
+
+            dataManager.Setup(x => x.AddMatches(
+                    It.Is<int>(i => i != ValidId)))
+                .Returns<Data.Models.Match>(null);
+
+            dataManager.Setup(x => x.AddMatches(
+                    It.Is<int>(i => i == ValidId)))
+                .Returns(TestObjectFactoryDataModels.Matches);
+
+            return dataManager.Object;
+        }
+    }
+}
