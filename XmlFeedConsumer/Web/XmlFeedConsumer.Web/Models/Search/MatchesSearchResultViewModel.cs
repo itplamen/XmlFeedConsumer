300324de55e315f27a54d6b8acecea506fc47c09
itplamen@gmail.com <itplamen@gmail.com>
2017-05-10 03:08:23 +0300

    Created MatchesSearchViewModel and MatchesSearchResultViewModel

---
+++ XmlFeedConsumer/Web/XmlFeedConsumer.Web/Models/Search/MatchesSearchResultViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace XmlFeedConsumer.Web.Models.Search
+{
+    using System.Collections.Generic;
+
+    using Home;
+    using Infrastructure.Mapping;
+
+    public class MatchesSearchResultViewModel : IMapFrom<MatchViewModel>
+    {
+        public IEnumerable<MatchViewModel> Matches { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
---
+++ XmlFeedConsumer/Web/XmlFeedConsumer.Web/Models/Search/MatchesSearchViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace XmlFeedConsumer.Web.Models.Search
+{
+    using XmlFeedConsumer.Common;
+
+    public class MatchesSearchViewModel
+    {
+        public MatchesSearchViewModel()
+        {
+            if (string.IsNullOrEmpty(this.SortBy))
+            {
+                this.SortBy = Constants.MatchesInitialOrderBy;
+            }
+
+            if (string.IsNullOrEmpty(this.SortType))
+            {
+                this.SortType = Constants.AscendingSortMatches;
+            }
+        }
+
+        public string SearchWord { get; set; }
+
+        public string SortBy { get; set; }
+
+        public string SortType { get; set; }
+    }
+}
